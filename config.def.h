675189bfe127ae277f0302aed886265e28d75256
Zion Lee <bicoljoy12@gmail.com>
2021-08-13 16:44:56 +0900

    custom-config

--- config.def.h
+++ config.def.h
@@ -72,12 +72,18 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *screenshotcmd[] = { "/home/zone/scripts/screenshot", NULL };
+static const char *volumecmd[] = { "/home/zone/scripts/volume", NULL };
+static const char *volumeupcmd[] = { "/home/zone/scripts/volumeup", NULL };
+static const char *volumedowncmd[] = { "/home/zone/scripts/volumedown", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = screenshotcmd } },
+	{ MODKEY|ShiftMask,             XK_equal,  spawn,          {.v = volumeupcmd } },
+	{ MODKEY,                       XK_minus,  spawn,          {.v = volumedowncmd } },
+	{ MODKEY,                       XK_equal,  spawn,          {.v = volumecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
